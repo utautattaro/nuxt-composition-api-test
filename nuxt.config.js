868c58ab05cb921cc035b0318873861f11c23092
ryotaro <utautattaro@users.noreply.github.com>
2021-09-03 00:57:55 +0900

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - sdtc',
+    titleTemplate: '%s - nuxt-composition-api',
     title: 'nuxt-composition-api',
     htmlAttrs: {
       lang: 'en'
